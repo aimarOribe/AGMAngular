73a79be26491b0db9057132cbdc5bdde6c8c1245
Giancarlo <markcarranzafer12@gmail.com>
2022-03-04 10:59:13 -0500

    Web page end

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -15,6 +15,7 @@ export class IndexComponent implements OnInit {
   ngOnInit(): void {
     
     let carrusel:any = document.querySelector(".carrusel-items");
+    
     let maxScrollLeft = carrusel.scrollWidth - carrusel.clientWidth;
     let intervalo:any
     let step = 1;
@@ -42,6 +43,36 @@ export class IndexComponent implements OnInit {
     });
     
     start();
+
+    let carruselDestacados:any = document.querySelector(".carrusel-items-destacados");
+
+    let maxScrollLeftDestacados = carruselDestacados.scrollWidth - carruselDestacados.clientWidth;
+    let intervaloDestacados:any
+    let stepDestacados = 1;
+    const startDestacados = () => {
+      intervaloDestacados = setInterval(function () {
+        carruselDestacados.scrollLeft = carruselDestacados.scrollLeft + stepDestacados;
+        if (carruselDestacados.scrollLeft === maxScrollLeftDestacados) {
+          stepDestacados = stepDestacados * -1;
+        } else if (carruselDestacados.scrollLeft === 0) {
+          stepDestacados = stepDestacados * -1;
+        }
+      }, 10);
+    };
+
+    const stopDestacado = () => {
+      clearInterval(intervaloDestacados);
+    };
+    
+    carruselDestacados.addEventListener("mouseover", () => {
+      stopDestacado();
+    });
+    
+    carruselDestacados.addEventListener("mouseout", () => {
+      startDestacados();
+    });
+    
+    startDestacados();
             
 
   }
